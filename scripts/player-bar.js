cf874319361b661b4cd0910ea5e09bc64e9f8780
Savannah Carlin <savannahcarlin@Savannahs-MacBook-Pro.local>
2018-04-25 12:44:18 -0400

    adding song progress slider

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -10,11 +10,28 @@
     }
     const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
     const nextSongIndex = currentSongIndex + 1;
-    if (nextSongIndex >= album.songs.length) { return; }
+    if (nextSongIndex >= album.songs.length) {
+      return;
+    }
 
     const nextSong = album.songs[nextSongIndex];
     player.playPause(nextSong);
   });
+
+  $('#time-control input').on('input', function(event) {
+    player.skipTo(event.target.value);
+  });
+
+  setInterval(() => {
+    if (player.playState !== 'playing') {
+      return;
+    }
+    const currentTime = player.getTime();
+    const duration = player.getDuration();
+    const percent = (currentTime / duration) * 100;
+    $('#time-control .current-time').text( currentTime );
+    $('#time-control input').val(percent);
+  }, 1000);
 
   $('button#previous').on('click', function() {
     if (player.playState !== 'playing') {
@@ -27,3 +44,4 @@
     player.playPause(previousSong);
   });
 }
+//
